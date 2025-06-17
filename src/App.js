172dfc6eae5935ecc9539af5d5ca0e18e9a75e8c
Emery <emannchew@gmail.com>
2025-06-17 19:54:17 -0400

    adding all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import './App.css';
+import Sam1 from './assets/sam1.jpg';
+import Sam2 from './assets/sam2.jpg';
+import Sam3 from './assets/sam3.jpg';
+import Sam4 from './assets/sam4.jpg';
+import Sam5 from './assets/sam5.jpg';
+import Sam6 from './assets/sam6.webp';
+
+const images = [
+  Sam1,
+  Sam2,
+  Sam3,
+  Sam4,
+  Sam5,
+  Sam6
+  // Add more photo paths here
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="carousel">
+        <Swiper spaceBetween={50} slidesPerView={1}>
+          {images.map((src, idx) => (
+            <SwiperSlide key={idx}>
+              <img src={src} alt={`Slide ${idx}`} className="slide-img" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
+    
   );
 }
 
